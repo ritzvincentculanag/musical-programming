5f7e9b27e1c042092f7563b9cd198e7283d8f7d2
ritznotcracker <ritznotcracker@gmail.com>
2022-06-06 22:54:23 +0800

    display: images on screen

--- main.py
+++ main.py
@@ -1,59 +1,35 @@
-# General imports
-import time
-
-import mediapipe as mp
-import numpy as np
-import pygame
 import cv2
-import uuid
+import time
 import os
 
-# Selective imports
-from pygame import mixer
 
-# Prepare things
-mp_drawing = mp.solutions.drawing_utils
-mp_hands = mp.solutions.hands
+# Capture device settings
+cap_w, cap_h = 640, 640
 
-# Sound configuration
-mixer.init()
-mixer.music.load('./assets/ting.mp3')
-mixer.music.set_volume(0.5)
-
-# Get video capture device
 cap = cv2.VideoCapture(0)
+cap.set(3, cap_w)
+cap.set(4, cap_h)
 
-# Use Hands in the mp+hands variable with minimum tracking confidence of 50% and
-# Minimum detection confidence of 80% and name it as `hands`.
-with mp_hands.Hands(min_tracking_confidence=0.8, min_detection_confidence=0.5) as hands:
-    while cap.isOpened():
-        ret, frame = cap.read()
-        mixer.music.play()
+# Image golder settings
+folder_path = 'images'
+images = os.listdir(folder_path)
+images.sort()
+overlays = []
 
-        # BGR 2 RGB
-        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        image = cv2.flip(image, 1)
-        image.flags.writeable = False
+for img_path in images:
+    img = cv2.imread(f'{folder_path}/{img_path}')
+    overlays.append(img)
 
-        # Render
-        results = hands.process(image)
 
-        # RGB 2 BGR
+#
 
-        image.flags.writeable = True
-        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
-        print(results)
+while True:
+    success, img = cap.read()
 
-        # Rendering results
-        if results.multi_hand_landmarks:
-            for num, hand in enumerate(results.multi_hand_landmarks):
-                mp_drawing.draw_landmarks(image, hand, mp_hands.HAND_CONNECTIONS)
+    h, w, c = overlays[1].shape
+    img[0:h, 0:w] = overlays[1]
 
-        cv2.imshow('Hand Tracking', image)
-
-        if cv2.waitKey(10) & 0xFF == ord('q'):
-            break
-
-cap.release()
-cv2.destroyAllWindows()
+    cv2.imshow('Musical Programming', img)
+    if cv2.waitKey(10) & 0xFF == ord('q'):
+        break
