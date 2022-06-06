f84e14f6acf635c7c0e4846bad35778694f09ca8
ritznotcracker <ritznotcracker@gmail.com>
2022-06-06 22:58:49 +0800

    display: fps to screen

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import cv2
 import time
 import os
 
-
 # Capture device settings
 cap_w, cap_h = 640, 640
 
@@ -20,9 +19,8 @@ for img_path in images:
     img = cv2.imread(f'{folder_path}/{img_path}')
     overlays.append(img)
 
-
-#
-
+# FPS settings
+previous_time = 0
 
 while True:
     success, img = cap.read()
@@ -30,6 +28,21 @@ while True:
     h, w, c = overlays[1].shape
     img[0:h, 0:w] = overlays[1]
 
+    # Set FPS
+    current_time = time.time()
+    fps = 1 / (current_time - previous_time)
+    previous_time = current_time
+
+    # Render to screen
+    cv2.putText(
+        img=img,
+        text=f'FPS: {int(fps)}',
+        org=(500, 40),
+        fontFace=cv2.QT_FONT_NORMAL,
+        fontScale=1,
+        color=(255, 0, 0),
+        thickness=3
+    )
     cv2.imshow('Musical Programming', img)
     if cv2.waitKey(10) & 0xFF == ord('q'):
         break
